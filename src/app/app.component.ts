e3fc4146ff1e93cd0d79ec76c11cfd5f93fe1757
mohammed ouchen <medouchen19@gmail.com>
2023-02-05 21:22:07 +0100

    add  list of todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  task = ''
+  tasks = [
+          { id: 1, value: 'to do 1'},
+  ]
+
+  addTask () {
+    this.tasks.push({ id: this.tasks.length+1, value: this.task})
+    this.task = '';
+  }
+
+  editTask(index: any) {
+    console.log('edit index =>', index);
+  }
+
+  deleteTask (index: any) {
+    console.log('delete Task => ', index);
+    this.tasks.splice(index, 1)
+  }
 }
